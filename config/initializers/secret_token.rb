d21f852e9d7a707488407fa9c24a125745ca80a2
Swidjit <alex@swidjit.com>
2013-04-05 12:27:07 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sayvit::Application.config.secret_token = '4ecad8835d3a3ab5a5405947e68d8a54ecc5596ca0e2aadb922ce63eabc0277eb420cdac56e40d6632419dbed88b0a975e5619bd0e8e1e580a56976bfe371f71'
