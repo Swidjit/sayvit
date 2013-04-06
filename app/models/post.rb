8bdedc4f19f7b0f421b308c61ccd73937e1c33f7
Barnett Klane <blklane@syr.edu>
2013-04-06 18:55:54 -0400

    fixing aws

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,9 @@ class Post < ActiveRecord::Base
   attr_accessible :title, :content, :image
 
   belongs_to :user
-  has_attached_file :image, :styles => {:thumb => 'x100', :croppable => '600x600>', :big => '1000x1000>'}
+  has_attached_file :image, :styles => {:thumb => 'x100', :croppable => '600x600>', :big => '1000x1000>',
+                      :url => ":s3_domain_url",
+                      :path => "/:class/images/:id_:basename.:style.:extension"}
 
   has_many :labels
   
