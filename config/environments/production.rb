43bc718d13d7448bb5b98b5cd56a63f1544d0883
Barnett Klane <blklane@syr.edu>
2013-04-06 19:02:34 -0400

    fixing aws part 5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,8 +64,6 @@ Sayvit::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  #heroku pg  
-  RAILS_ENV=production bundle exec rake assets:precompile
 
   #AWS paperclip
   config.paperclip_defaults = {
