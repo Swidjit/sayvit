0f7ce0fceee530e18eb6428f930e55c2835e85ef
Barnett Klane <blklane@syr.edu>
2013-04-06 18:10:27 -0400

    added aws

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,12 @@ class Post < ActiveRecord::Base
   has_attached_file :image, :styles => {:thumb => 'x100', :croppable => '600x600>', :big => '1000x1000>'}
 
   ## acts_as_taggable
+
+  auto_html_for :body do
+    html_escape
+    image
+    youtube(:width => 400, :height => 250)
+    link :target => "_blank", :rel => "nofollow"
+    simple_format
+  end
 end
