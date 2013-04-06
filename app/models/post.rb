ed293d3c2b20412eafbb93575150ce1a520b731b
Barnett Klane <blklane@syr.edu>
2013-04-06 19:30:20 -0400

    aws testing

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,6 @@ class Post < ActiveRecord::Base
 
   belongs_to :user
   has_attached_file :image, :styles => {:thumb => 'x100', :croppable => '600x600>', :big => '1000x1000>',
-                      :url => ":s3_domain_url",
                       :path => "/:class/images/:id_:basename.:style.:extension"}
 
   has_many :labels
