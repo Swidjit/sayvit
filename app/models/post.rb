16407baaa0b882b96a80577033daa1e038467627
Barnett Klane <blklane@syr.edu>
2013-04-06 17:39:16 -0400

    fixed new post creation

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,5 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_attached_file :image, :styles => {:thumb => 'x100', :croppable => '600x600>', :big => '1000x1000>'}
 
-  validates_presence_of :title, :content
-  acts_as_taggable
+  ## acts_as_taggable
 end
