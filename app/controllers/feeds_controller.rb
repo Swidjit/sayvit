71353dc794e449c2824ee45a2edcd8e8e89fc6f7
Swidjit <alex@swidjit.com>
2013-04-06 20:53:24 -0400

    tag feeds

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -3,6 +3,7 @@ class FeedsController < ApplicationController
   def home
     if params[:tag].present?
       @feed_posts = Post.tagged_with(params[:tag])
+      @tag = params[:tag]
     else
       @feed_posts = Post.all 
     end
